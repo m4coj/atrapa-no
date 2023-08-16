98d300a3018fae7ad485836c049dab08373ada5f
JMC <137670649+m4coj@users.noreply.github.com>
2023-08-16 17:41:14 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('JAJAJA, AQUI NO DICE NO, NO SEAS BOT')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
